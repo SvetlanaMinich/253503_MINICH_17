164b14f1609a0b865bf7eec97e1383079ee9098f
Svetlana Minich <imsvetam@mail.ru>
2024-10-20 21:57:09 +0300

    lab2 started

--- strweb/LR1/hello_django/djangoenv/start/start/settings.py
+++ strweb/LR1/hello_django/djangoenv/start/start/settings.py
@@ -16,11 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS= [
-    os.path.join(BASE_DIR, "static")
-]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -128,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
